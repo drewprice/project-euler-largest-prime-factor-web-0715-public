e5994694a63fda68ec85fc70c73e245608b0ffe7
Drew Price <drewprice@users.noreply.github.com>
2015-10-12 12:36:08 -0400

    Sort of working... too slow

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,17 @@
-# Enter your procedural solution here!
+def largest_prime_factor(n)
+  candidate = n / 2
+  candidate -= 1 if candidate.even?
+
+  while candidate > 1
+    return candidate if factor?(n, candidate) && prime?(candidate)
+    candidate -= 2
+  end
+end
+
+def prime?(n)
+  (3..n / 2).step(2).none? { |i| n % i == 0 }
+end
+
+def factor?(n, candidate)
+  n % candidate == 0
+end
